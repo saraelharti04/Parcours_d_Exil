d87232d343f935c8ef998996622bb1aec16a178e
EL HARTI Sara <sara.el-harti@student-cs.fr>
2025-06-09 20:14:28 +0200

    pubsepc

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Thibault\AppData\Local\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Thibault\Desktop\CS\Projet\appbddappwrite\parcours_d_exil"
+export "FLUTTER_ROOT=C:\Users\sarae\Desktop\parcours_exil_app\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\sarae\AndroidStudioProjects\MyApplication4\Parcours_d_Exil"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.2"
-export "FLUTTER_BUILD_NUMBER=1.0.2"
+export "FLUTTER_BUILD_NAME=1.0.3"
+export "FLUTTER_BUILD_NUMBER=3"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
